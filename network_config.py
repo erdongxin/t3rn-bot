b7d861bbaec5c41bd391d3567f1d46264c3c951e
erdongxin <1007422542@qq.com>
2025-03-26 01:39:07 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -7,7 +7,7 @@ networks = {
         'contract_address':'0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3'
     },
     'OP Sepolia': {
-        'rpc_url':'https://sepolia.optimism.io​',
+        'rpc_url':'https://endpoints.omniatech.io/v1/op/sepolia/public​',
         'chain_id':11155420,
         'contract_address':'0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     },
