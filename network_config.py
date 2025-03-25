3f0e4d8054adb5bdfce7fee214f32fa9f7ee1d0f
erdongxin <1007422542@qq.com>
2025-03-26 01:17:05 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -2,13 +2,13 @@
 
 networks = {
     'Base': {
-        'rpc_url': 'https://base-sepolia.g.alchemy.com/v2/VoylN1mrsDm0VfjPCT9yl7kJfB6MH9Em', 
+        'rpc_url': 'https://sepolia.base.org', 
         'chain_id': 84532, 
         'contract_address': 
         '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3' 
     },
     'OP Sepolia': {
-        'rpc_url': 'https://opt-sepolia.g.alchemy.com/v2/VoylN1mrsDm0VfjPCT9yl7kJfB6MH9Em',
+        'rpc_url': '​https://sepolia.optimism.io',
         'chain_id': 11155420,
         'contract_address': '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     },
