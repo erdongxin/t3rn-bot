4194f00ef707387367e1a0f04ef7b17e1747e2fc
erdongxin <1007422542@qq.com>
2025-04-08 22:54:48 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,7 @@ def check_balance(web3, my_address):
 # 创建和发送交易的函数
 def send_bridge_transaction(web3, account, my_address, data, network_name):
     nonce = web3.eth.get_transaction_count(my_address, 'pending')
-    value_in_ether = 3.0
+    value_in_ether = 1.0
     value_in_wei = web3.to_wei(value_in_ether, 'ether')
 
     try:
@@ -95,7 +95,7 @@ def send_bridge_transaction(web3, account, my_address, data, network_name):
             'data': data,
             'value': value_in_wei
         })
-        gas_limit = gas_estimate + 50000
+        gas_limit = gas_estimate + 100000
     except Exception as e:
         print(f"估计gas错误: {e}")
         return None  # 直接返回 None 表示完全失败
@@ -249,8 +249,8 @@ def main():
 
                 # 检查当前网络余额是否足够
                 balance = check_balance(web3, my_address)
-                if balance < 3.01:
-                    print(f"{chain_symbols[current_network]}⚠️ {my_address} 在 {current_network} 余额不足 3.01 ETH，尝试切换到 {alternate_network}{reset_color}")
+                if balance < 1.01:
+                    print(f"{chain_symbols[current_network]}⚠️ {my_address} 在 {current_network} 余额不足 1.01 ETH，尝试切换到 {alternate_network}{reset_color}")
 
                     try:
                         alt_web3 = create_web3_connection(alternate_network)
@@ -259,7 +259,7 @@ def main():
                         print(f"备用网络检查失败: {e}")
                         continue
 
-                    if alt_balance >= 3.01:
+                    if alt_balance >= 1.01:
                         new_network = address_state.switch_network(my_address)
                         current_network = new_network
                         web3 = alt_web3
