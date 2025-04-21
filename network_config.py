a1cac1c4a6791d86540bd8276b9b4268ade1ee56
erdongxin <1007422542@qq.com>
2025-04-21 21:14:43 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -57,12 +57,14 @@ rpc_api_keys = [
 
 networks = {
     'Base': {
-        'rpc_urls': [f'https://base-sepolia.g.alchemy.com/v2/{key}' for key in rpc_api_keys],
+        #'rpc_urls': [f'https://base-sepolia.g.alchemy.com/v2/{key}' for key in rpc_api_keys],
+        'rpc_urls': 'https://sepolia.base.org',
         'chain_id': 84532,
         'contract_address': '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3'
     },
     'OP Sepolia': {
-        'rpc_urls': [f'https://opt-sepolia.g.alchemy.com/v2/{key}' for key in rpc_api_keys],
+        #'rpc_urls': [f'https://opt-sepolia.g.alchemy.com/v2/{key}' for key in rpc_api_keys],
+        'rpc_urls': 'https://sepolia.optimism.io',
         'chain_id': 11155420,
         'contract_address': '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     }
